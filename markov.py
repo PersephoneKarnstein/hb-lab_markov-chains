2f39fd3ce8b36f004d7fd5d9a910af906c9d9d04
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-16 18:03:26 -0700

    can use stdin for files, also n-grams of size n

--- markov.py
+++ markov.py
@@ -1,7 +1,7 @@
 """Generate Markov text from text files."""
 
 from random import choice
-
+import sys
 
 def open_and_read_file(file_path):
     """Take file path as string; return text as string.
@@ -39,50 +39,62 @@ def make_chains(text_string):
         >>> chains[('there','juanita')]
         [None]
     """
-
-    # set of all unique bigrams
-    chains = set()
+    # dictionary also only contains unique keys
+    # chains = {}
     
     text_string = text_string.split()
     # print(text_string)
-    for i in range(len(text_string)-1):
-        chains.add((text_string[i], text_string[i+1]))
-    # print(chains)
 
+    # further study: alter the size of n-gram used
+    ngram_size = 3 #for a bigram, want text_string[0:n-1]
     chain_dict = {}
 
-    # loop through every ngram in our set of ngrams
-    for ngram in chains:
-        # print('ngram:', ngram)
-        words_following = []
+    # loop through each word in text string array, and add tuple of current word + next word into chains set
+    for i in range(len(text_string)-ngram_size+1):
+        # chains.add((text_string[i], text_string[i+1]))
+        # chains.add((text_string[i], text_string[i+ngram_size-1]))
+        try:
+            words_following = text_string[i+ngram_size]
+        except IndexError: words_following = None
+        # print(words_following)
+        ngram = tuple(text_string[i:i+ngram_size])
+        # print(ngram)
+        ## why doesn't append.words_following work?
+        chain_dict[ngram] = chain_dict.get(ngram, []) + [words_following]
+        # print(chain_dict)
+        
 
-        # loop through every string in the split original text string (['Would'], ['you'],...)
-        ## stop at len - 1 so we don't get an error once we hit the last word
-        for i in range(len(text_string)-1):
-            try:
-                if text_string[i]+text_string[i+1] == ngram[0]+ngram[1]:
-                    # print(text_string[i+2])
-                    words_following.append(text_string[i+2])
-                    # print('words following:', words_following)
-                # chain_dict.add({ngram:words_following})
-            except IndexError: pass
-        chain_dict[ngram] = words_following
+    # # loop through every ngram in our set of ngrams/key
+    # for ngram in chains:
+    #     # print('ngram:', ngram)
+    #     words_following = []
+    #     # loop through every string in the split original text string (['Would'], ['you'],...)
+    #     ## stop at len - 1 so we don't get an error once we hit the last word
+    #     for i in range(len(text_string)-1):
+    #         try:
+    #             if " ".join(text_string[i:i+ngram_size]) == " ".join(ngram[:ngram_size]):
+    #                 # print(text_string[i+2])
+    #                 words_following.append(text_string[i+ngram_size])
+    #                 # print('words following:', words_following)
+    #         except IndexError: pass
+        # chain_dict[ngram] = words_following
 
-    # for mkv in chain_dict: print(mkv, chain_dict[mkv])
-
+    for mkv in chain_dict: print(mkv, ":", chain_dict[mkv])
 
     return chain_dict
-
 
 def make_text(chains):
     """Return text from chains."""
 
     our_string = []
     ## current_tuple = ('could', 'in') ## use choice on list of keys to avoid hardcoding
+    # print(list(chains.keys()))
     current_tuple = choice(list(chains.keys()))
+    # print(f"\ncurrent tuple = {current_tuple}")
+    # print(f"dictionary entry = {chains[current_tuple]}")
     nextword = "llama"
 
-    our_string = current_tuple[0]+" "+current_tuple[1]
+    our_string = " ".join(current_tuple)
 
 
     while nextword != None:
@@ -90,15 +102,23 @@ def make_text(chains):
         try:
             nextword = choice(chains[current_tuple])
 
-            next_tuple = (current_tuple[1], nextword)
+            # print("----------------------------------------\n",
+            #     f"current tuple = {current_tuple}\n",
+            #      f"dictionary entry = {chains[current_tuple]}\n",
+            #      f"next word = {nextword}\n")
+
+            next_tuple = (*current_tuple[1:], nextword)
+            # next_tuple = current_tuple[1:] + (nextword,)
+
             our_string += f" {nextword}"
             current_tuple = next_tuple
         except IndexError: break
+        # except KeyError: break
     # print(our_string)
     return our_string
 
 
-input_path = "gettysburg.txt"
+input_path = sys.argv[1]
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
